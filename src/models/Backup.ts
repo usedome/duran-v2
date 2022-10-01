d64d0d01c529afdd34bb8961fe2f2b399daedc0b
olamileke <olamileke.f@gmail.com>
2022-10-01 22:39:01 +0100

    feat: finished model setup

---
+++ src/models/Backup.ts
@@ -0,0 +1,24 @@
+import { Schema, model } from "mongoose";
+import { TResource } from "./Resource";
+
+export type TBackup = {
+  _id: Schema.Types.ObjectId;
+  uuid: string;
+  resource: TResource;
+  url: string;
+  created_at: Date;
+  updated_at: Date;
+};
+
+const backupSchema = new Schema<TBackup>(
+  {
+    uuid: { type: String },
+    resource: { type: Schema.Types.ObjectId, ref: "Resource", required: true },
+    url: { type: String, required: true },
+  },
+  {
+    timestamps: { createdAt: "created_at", updatedAt: "updated_at" },
+  }
+);
+
+export const Backup = model<TBackup>("Backup", backupSchema);
---
+++ src/models/index.ts
@@ -0,0 +1,4 @@
+export * from "./Backup";
+export * from "./Resource";
+export * from "./Service";
+export * from "./User";
