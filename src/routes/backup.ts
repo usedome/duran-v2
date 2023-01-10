ce44971c21a2d5cc1316367a214e0b47090e9926
olamileke <olamileke.f@gmail.com>
2023-01-10 21:35:54 +0100

    fix: fixed issues with emails

--- src/routes/backup.ts
+++ src/routes/backup.ts
@@ -13,6 +13,20 @@ import { check, body } from "express-validator";
 export const backupRouter = express.Router();
 
 backupRouter.post(
+  "/",
+  fileMiddleware.single("backup"),
+  check("backup")
+    .custom((_, { req }) => {
+      if (!req?.file) return false;
+      return Boolean(req?.file?.mimetype);
+    })
+    .withMessage("backup is required"),
+  body("format", "format is required").exists(),
+  createBackupMiddleware,
+  createBackup
+);
+
+backupRouter.post(
   "/:resource_uuid",
   fileMiddleware.single("backup"),
   check("backup")
