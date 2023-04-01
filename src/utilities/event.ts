75625fc56a24c99a8ba14505a8ad34491dceb712
olamileke <olamileke.f@gmail.com>
2023-04-01 21:45:18 +0100

    feat: implemented ip address check

--- src/utilities/event.ts
+++ src/utilities/event.ts
@@ -1,6 +1,7 @@
 import { EventEmitter } from "events";
 import {
   backupSuccessfulListener,
+  backupUnauthorizedIpListener,
   backupWrongCredentialsListener,
 } from "../listeners";
 
@@ -9,4 +10,5 @@ export const eventEmitter = new EventEmitter();
 export const initEvents = () => {
   eventEmitter.on("backup.successful", backupSuccessfulListener);
   eventEmitter.on("backup.wrong.credentials", backupWrongCredentialsListener);
+  eventEmitter.on("backup.unauthorized.ip", backupUnauthorizedIpListener);
 };
