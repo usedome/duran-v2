46c87460a399fb022377b3dfa573c895cb7c8a3b
olamileke <olamileke.f@gmail.com>
2023-12-24 01:20:24 +0100

    feat: added ping api route

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { backupRouter, resourceRouter, serviceRouter } from "./routes";
 import { bootstrap, loadEnv, initEvents } from "./utilities";
+import { Request, Response, NextFunction } from "express";
 
 // Loading up relevant environment variables
 loadEnv();
@@ -12,6 +13,9 @@ initEvents();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.post("/ping", (_: Request, response: Response, __: NextFunction) => {
+  return response.status(200).json({ status: "ok" });
+});
 app.use(backupRouter);
 app.use(resourceRouter);
 app.use(serviceRouter);
